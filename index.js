f6b549dc5137da436b0439298c43e3bfd596af88
Msmoinsk <moinsk5365@gmail.com>
2024-03-01 16:16:31 +0530

    Starting the JavaScript in project

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var state = {
-    tasklist : [
-        {
-            image_url : "",
-            task_title : "",
-            task_type : "",
-            task_desc : ""
-        }
-    ]
-}
+// var state = {
+//     tasklist : [
+//         {
+//             image_url : "",
+//             task_title : "",
+//             task_type : "",
+//             task_desc : ""
+//         }
+//     ]
+// }
+
+const state = {
+    tasklist : [],
+};
+
+// DOM Operations here.......................
+const taskContent = document.querySelector('.task__contents');
+const taskBody = document.querySelector('.task__modal__body');
+
+// This is for understanding purpose
+// console.log(taskContent)
+// console.log(taskBody)
+
+const htmlTaskContent = ({id,title,desription,type,url}) => {}
