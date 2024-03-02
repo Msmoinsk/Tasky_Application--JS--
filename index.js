67d9b63ffc2d46fedaf5c963c1a8ed901ae50a5f
Msmoinsk <moinsk5365@gmail.com>
2024-03-03 01:01:24 +0530

    Cards are now gerating on the add button

--- index.js
+++ index.js
@@ -9,10 +9,7 @@
 //     ]
 // }
 
-// Building the Cards logic
-const state = {
-    tasklist : [],
-};
+// STARTING OF THE MAIN LOGIC------------------------------------------------------------------
 
 // DOM Operations here.......................
 const taskContent = document.querySelector('.task__contents');
@@ -24,26 +21,27 @@ const taskBody = document.querySelector('.task__modal__body');
 
 // Template for the cards on screen
 // `` - this is just like the {} but i used it for HTML writing
-const htmlTaskContent = ({ id, title, desription, type, url}) => `
+const htmlTaskContent = ({ id, title, description, type, url}) => `
     <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
         <div class="card shadow task__card">
 
             <div class="card-header d-flex justify-content-end task__card__header">
-                <button type="button" class="btn btn-outline-info mr-1.5" name=${id}>
+                <button type="button" class="btn btn-outline-info m-2" name=${id}>
                     <i class="fa-solid fa-pencil" name=${id}></i>
                 </button>
-                <button type="button" class="btn btn-outline-danger mr-1.5" name=${id}>
+                <button type="button" class="btn btn-outline-danger m-2" name=${id}>
                     <i class="fa-solid fa-trash-can" name=${id}></i>
                 </button>
             </div>
 
             <div class="card-body">
                 ${
-                    url &&
-                    `<img width='100%' src='${url}' alt='Card Image' class='card-img-top rounded-lg' />`
+                    url 
+                    ? `<img width='100%' src='${url}' alt='Card Image' class='card-img-top rounded-lg' />`
+                    : `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src="https://tse3.mm.bing.net/th?id=OIP.LZsJaVHEsECjt_hv1KrtbAHaHa&pid=Api&P=0" alt='card image cap' class='card-image-top md-3 rounded-lg' />`
                 }
                 <h4 class="card-title task__card__title">${title}</h4>
-                <p class="descriptions trim-3-lines text-muted">${desription}</p>
+                <p class="descriptions trim-3-lines text-muted">${description}</p>
                 <div class="Type text-white d-flex flex-wrap">
                     <span class="badge rounded-pill text-bg-primary">${type}</span>
                 </div>
@@ -58,21 +56,30 @@ const htmlTaskContent = ({ id, title, desription, type, url}) => `
 `;
 
 // Modal body on >> Click Of open
-const htmlModelContent = ({ id, title, desription, url}) => {
+const htmlModelContent = ({ id, title, description, url}) => {
     const date = new Date(parseInt(id));
     return `
         <div id=${id}>
             ${
-                url &&
-                `<img width='100%' src='${url}' alt='Card Image' class='img-fluid' place__holder__img mb-3 />`
+                url 
+                ? `<img width='100%' src='${url}' alt='Card Image' class='card-img-top rounded-lg' />`
+                : `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src="https://tse3.mm.bing.net/th?id=OIP.LZsJaVHEsECjt_hv1KrtbAHaHa&pid=Api&P=0" alt='card image cap' class='card-image-top md-3 rounded-lg' />`
             }
             <strong class="text-muted text-sm">Created on : ${date.toDateString()}</strong>
             <h3 class="my-3">${title}</h3>
-            <p class="text-muted">${desription}</p>
+            <p class="text-muted">${description}</p>
         </div>
     `
 };
 
+// 1. Building the Cards logic [ Main Storage ]
+// Its a BACKUP storage
+const state = {
+    tasklist : [],
+};
+
+// 2. its a PRIMARY storage
+// if the localStorage is cleared then their will be no data
 const updateLocalStorage = () => {
     localStorage.setItem(
         'task',
@@ -81,3 +88,40 @@ const updateLocalStorage = () => {
         })
     )
 }
+
+// 3. Load the data & insert to the [STATE] variable
+// It will Work after reload : as it help to keep data permanent
+const LoadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.getItem('task'))
+    if(localStorageCopy) state.tasklist = localStorageCopy.tasks;
+
+    state.tasklist.map((cardDate) => {
+        taskContent.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate))
+    })
+}
+
+// 4. Onn Button Click Funcion
+// When the card data given :===> Submit
+const handleSubmit = (event) => {
+    const id = `${Date.now()}`;
+    const input = {
+        url: document.getElementById('imageurl').value,
+        title: document.getElementById('Title').value,
+        type: document.getElementById('Type').value,
+        description: document.getElementById('Description').value
+    }
+
+    // display in front of ui
+    taskContent.insertAdjacentHTML("beforeend",htmlTaskContent({
+        id,
+        ...input
+    }))
+    // Backup data 
+    state.tasklist.push({...input, id})
+
+    // adding it to localStorage
+    updateLocalStorage();
+}
+
+// 5. Call LoadInitialData when the page loads
+window.addEventListener('load', LoadInitialData);
